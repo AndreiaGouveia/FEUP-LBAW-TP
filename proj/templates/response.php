924a9d4b719be28a7fa68d7f7535b85a93e2b3bd
filipasenra <filipasenra4@gmail.com>
2020-03-04 11:08:37 +0000

    Adding comments and responses (not finish)

--- proj/templates/response.php
+++ proj/templates/response.php
@@ -11,11 +11,14 @@ function drawResponseSection()
         drawResponse("Mea sale accusam id. Sed eros corpora at. Justo definitionem sea at, eu unum vidit usu. Saepe voluptatum pro at. Possit pertinax argumentum mei in. An etiam alterum corpora sed, an impetus nominati his. Oratio nullam vidisse in nam.
 
         No dictas commune delicata eum. Ex feugiat omittam cum, vix ut hinc commune. Ut nec unum mazim tempor, pro eu simul conclusionemque. Dicant vituperata duo ad, ad wisi nullam vim, rebum oratio electram ei usu. Sed suas dicit possim id.", "No Qui Elit");
+
         drawResponse("Oporteat salutatus ne nec. Tractatos quaerendum ad sed. His praesent elaboraret no, munere singulis partiendo eam ex. Ad mucius ponderum est. Nisl primis concludaturque ne qui, facete assentior vix id. Cu eius instructior sed.
 
         Tota diceret erroribus et pri. Duo detraxit partiendo cu. Ei est alienum blandit. Mei saepe dicant iuvaret ut.
         
-        Dicta appareat an eum, per ex alii similique.", "Sea Dicunt Oblique")
+        Dicta appareat an eum, per ex alii similique.", "Sea Dicunt Oblique");
+
+        drawResponse("Mea sale accusam id. Sed eros corpora at. Justo definitionem sea at, eu unum vidit usu. Saepe voluptatum pro at. Possit pertinax argumentum mei in. An etiam alterum corpora sed, an impetus nominati his. Oratio nullam vidisse in nam.", "No Qui Elit");
         ?>
 
     </ul>
