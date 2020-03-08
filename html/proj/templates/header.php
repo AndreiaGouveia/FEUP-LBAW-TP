660223138b8a8b96962bb626feecfd6cf5e67163
filipasenra <filipasenra4@gmail.com>
2020-03-08 13:26:48 +0000

    Added SearchBar

--- html/proj/templates/header.php
+++ html/proj/templates/header.php
@@ -2,42 +2,73 @@
 function drawHeaderMember()
 {
 ?>
-    <nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light mb-4">
-        <a class="navbar-brand mr-5" href="#">
+    <nav class="navbar navbar-expand sticky-top navbar-light bg-light mb-4">
+        <a class="navbar-brand" href="#">
             <img src="../logo.png" width="35" height="42" class="d-inline-block align-center" alt="">
             Papagaio
         </a>
-
-        <div class="navbar-collapse d-flex ml-5" id="navbarSupportedContent">
-
-            <form class="navbar-nav form-inline flex-fill">
-
-                <div class="input-group flex-fill mr-3">
-                    <input type="text" class="form-control flex-fill" placeholder="Pesquisa">
-                    <div class="input-group-append">
-                        <button class="btn btn-secondary" type="button">
-                            <i class="fa fa-search"></i>
-                        </button>
-                    </div>
-                </div>
-
-                <button type="button" class="btn btn-primary mr-4"><i class="fas fa-plus-circle mr-1"></i> Adicionar Questão</button>
-
-                <div class="btn-group">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-user"></i>
-                    </button>
-
-                    <div class="dropdown-menu  dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="settings.php">Settings</a>
-                    </div>
-                </div>
-
-            </form>
+        <!-- Visible in XL (screen of computer)-->
+        <div class="navbar-collapse"><div class="flex-fill d-none d-xl-block" id="navbarSupportedContent"><?php drawSideHeaderDesktop(); ?></div></div>
+        <!-- Visible in all other screens-->
+        <div class="d-xl-none m-0" id="navbarSupportedContent">
+            <?php drawSideHeaderMobile(); ?>
         </div>
     </nav>
 
-<?php } ?>
+<?php
+}
+
+function drawSideHeaderDesktop()
+{
+?>
+    <form class="navbar-nav form-inline flex-fill">
+        <div class="input-group flex-fill mr-3">
+            <input type="text" class="form-control flex-fill" placeholder="Pesquisa">
+            <div class="input-group-append">
+                <button class="btn btn-secondary" type="button">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
+        <button type="button" class="btn btn-primary mr-4"><i class="fas fa-plus-circle mr-1"></i> Adicionar Questão</button>
+        <div class="btn-group">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user"></i>
+            </button>
+            <div class="dropdown-menu  dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="settings.php">Settings</a>
+            </div>
+        </div>
+
+    </form>
+<?php
+}
+
+function drawSideHeaderMobile()
+{
+?>
+    <form class="navbar-nav form-inline d-flex justify-content-end">
+
+    
+        <button class="btn btn-secondary btn-sm mr-3" type="button"><i class="fa fa-search"></i></button>
+
+        <button type="button" class="btn btn-primary btn-sm mr-3"><i class="fas fa-plus-circle"></i></button>
+
+        <div class="btn-group">
+            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user"></i>
+            </button>
+
+            <div class="dropdown-menu  dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="settings.php">Settings</a>
+            </div>
+        </div>
+
+    </form>
+<?php
+}
+
+?>
 
 <?php
 function drawHeaderVisitor()
