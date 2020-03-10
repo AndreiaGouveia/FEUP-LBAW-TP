b8d431457694bd0c4cc8326e6e7e237a2dc3e363
filipasenra <filipasenra4@gmail.com>
2020-03-10 20:32:40 +0000

    Fixing mistake in cards

--- html/proj/templates/profile_activity.php
+++ html/proj/templates/profile_activity.php
@@ -56,9 +56,11 @@ function drawBasicActivity($date, $title, $description)
 function drawInfoBasicActivity()
 {
 ?>
-    <div class="row justify-content-between mt-3 px-0 mx-0">
-        <?php drawTopicsInCard(); ?>
-        <div class="info row justify-content-end mx-0">
+    <div class="row mt-3 px-0 mx-0">
+        <div class="info row justify-content-start d-line mx-0">
+            <?php drawTopicsInCard(); ?>
+        </div>
+        <div class="info flex-fill d-flex justify-content-end mx-0">
             <?php drawLikeButtons(); ?>
         </div>
 
