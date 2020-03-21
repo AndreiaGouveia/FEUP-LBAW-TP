944109c17d6c92f944a52d5b0cf6e7b8015f4b9a
Cláudia <claudiainescostamartins@gmail.com>
2020-03-21 22:22:49 +0000

    Updated SQL

--- html/proj/database/Papagaio.sql
+++ html/proj/database/Papagaio.sql
@@ -3,35 +3,105 @@
 CREATE TYPE medalType AS ENUM ('Gold', 'Silver', 'Bronze'); 
 CREATE TYPE report AS ENUM ( 'Spam' , 'Hate speach' , 'Terrorism' , 'Fake News' , 'Illegal Sales' , 'Violence' , 'Nudity' , 'Harassment' , 'Self Harm');
  
+
 -- Tables
- 
- 
+
 CREATE TABLE "person" (
-    idPerson SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username text NOT NULL CONSTRAINT person_username_uk UNIQUE,
     email text NOT NULL CONSTRAINT person_email_uk UNIQUE,
     password text NOT NULL,
 );
  
 CREATE TABLE administrator (
-    idPerson INTEGER PRIMARY KEY REFERENCES "person" (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    idPerson INTEGER PRIMARY KEY REFERENCES "person" (id) ON UPDATE CASCADE
 );
  
 CREATE TABLE member (
-    idMember INTEGER PRIMARY KEY REFERENCES "person" (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    idPerson INTEGER PRIMARY KEY REFERENCES "person" (id) ON UPDATE CASCADE ON DELETE CASCADE,
     name text NOT NULL,
     biography text,
-    idMedal INTEGER REFERENCES medal --falta o check e o resto
+    points DEFAULT 0 CONSTRAINT points_ck CHECK (points > 0),
+    idMedal INTEGER REFERENCES medal (id) CONSTRAINT medal_ck CHECK ((points<2020 AND idMedal == NULL) OR
+   (idMedal != NULL AND ((points > 2020 AND points <=3999 AND idMedal.type == 'Bronze') 
+   OR (points > 3999 AND points <=5999 AND idMedal.type == 'Silver') OR (points > 5999 AND idMedal.type == 'Gold')))),
+    idLocation INTEGER REFERENCES member_location (id) ON UPDATE CASCADE,
+    idPhoto INTEGER REFERENCES photo (id) CONSTRAINT photo_ck CHECK (photo.idPublication == NULL) ON UPDATE CASCADE
+);
+
+CREATE TABLE medal (
+    id SERIAL PRIMARY KEY,
+    TYPE medalType NOT NULL
 );
  
-CREATE TABLE location (
-    idLocation SERIAL PRIMARY KEY,
+CREATE TABLE memberLocation (
+    id SERIAL PRIMARY KEY,
     city text,
     district text,
     country text
 );
+
+CREATE TABLE photo (
+    id SERIAL PRIMARY KEY,
+    idPublication INTEGER  REFERENCES "publicaton" (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    url text
+);
+
+CREATE TABLE publication (
+    id SERIAL PRIMARY KEY,
+    city text NOT NULL,
+    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    idOwner INTEGER  REFERENCES "member" (id) NOT NULL
+);
+
+CREATE TABLE reported (
+    idMember INTEGER NOT NULL REFERENCES "member" (id) ON UPDATE CASCADE,
+    idPublication INTEGER NOT NULL REFERENCES "publication" (id) ON UPDATE CASCADE,
+    TYPE reason NOT NULL,
+    PRIMARY KEY (idMember, idPublication)
+);
+
+CREATE TABLE commentablePublication (
+    idPublication INTEGER PRIMARY KEY REFERENCES "publication" (id) ON UPDATE CASCADE
+);
+
+
  
-/* EXEMPLO DO STOR -- o nome das tabelas ficam entre aspas sempre que sao chamados em mais que uma tabela?
+/* -- Types
+ 
+CREATE TYPE media AS ENUM ('CD', 'DVD', 'VHS', 'Slides', 'Photos', 'MP3');
+ 
+-- Tables
+ 
+ 
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
+    email text NOT NULL CONSTRAINT user_email_uk UNIQUE,
+    name text NOT NULL,
+    obs text,
+    password text NOT NULL,
+    img text,
+    is_admin BOOLEAN NOT NULL
+);
+ 
+CREATE TABLE publisher (
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL
+);
+ 
+CREATE TABLE location (
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL,
+    address text NOT NULL,
+    gps text
+);
+ 
+CREATE TABLE author (
+    id SERIAL PRIMARY KEY,
+    name text NOT NULL,
+    img text
+);
+ 
 CREATE TABLE collection (
     id SERIAL PRIMARY KEY,
     name text NOT NULL
@@ -96,4 +166,5 @@ CREATE TABLE wish_list (
     id_work INTEGER NOT NULL REFERENCES "work" (id) ON UPDATE CASCADE,
     id_user INTEGER NOT NULL REFERENCES "user" (id) ON UPDATE CASCADE,
     PRIMARY KEY (id_work, id_user)
+);
 );*/
