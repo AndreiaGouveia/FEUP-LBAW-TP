63abe304c2928deae1acfc899753a85019908eaa
up201704077 <up201704077@fe.up.pt>
2020-04-23 20:25:43 +0100

    Added route to user profile from homepage

--- resources/views/partials/basic_activity.blade.php
+++ resources/views/partials/basic_activity.blade.php
@@ -1,7 +1,7 @@
 <a href="{{ route('show.question', $question->id) }}" class="hiperlink-in-activity">
     <div class="activity py-4 px-4 border-top">
 
-        @include('partials.header_activity', ['name' => $question->name, "link_profile" => $question->url, 'action' => "", 'actionInBold' => "", "date" => $question->date])
+        @include('partials.header_activity', ['idMember' => $question->memberId, 'name' => $question->name, "link_profile" => $question->url, 'action' => "", 'actionInBold' => "", "date" => $question->date])
         <h5 class="title"><?= $question->title  ?></h5>
         <p class="text"><?= $question->description ?></p>
 
--- resources/views/partials/header_activity.blade.php
+++ resources/views/partials/header_activity.blade.php
@@ -2,12 +2,14 @@
 $link = (isset($link_profile)) ? $link_profile : "https://i.stack.imgur.com/l60Hf.png";
 ?>
 
+<a href="{{ route('members', $idMember) }}" class="hiperlink-in-activity">
     <div id="header-card mb-3">
         <img src=<?= $link ?> class="img_inside mr-2" alt="">
         <div class="header-text">
-            <p class="name-and-action font-weight-bold d-inline"><?=$name?></p>
-            <p class="name-and-action d-inline"><?=$action?></p>
-            <p class="name-and-action font-weight-bold d-inline"><?=$actionInBold?></p><br>
-            <p><small><?=$date?></small></p>
+            <p class="name-and-action font-weight-bold d-inline"><?= $name ?></p>
+            <p class="name-and-action d-inline"><?= $action ?></p>
+            <p class="name-and-action font-weight-bold d-inline"><?= $actionInBold ?></p><br>
+            <p><small><?= $date ?></small></p>
         </div>
     </div>
+</a>
