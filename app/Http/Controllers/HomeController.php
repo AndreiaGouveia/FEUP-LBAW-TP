26844a89d655b4e536f29d48efb29512d7796ef8
Cláudia Martins <claudiainescostamartins@gmail.com>
2020-05-11 11:23:32 +0100

    CHanging filtered search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -220,25 +220,7 @@ class HomeController extends Controller
                         ->where('tag.name', 'ilike', '%' . $query . '%')
                         ->get();
 
-                if ($filter == 'relevant') {
-                        $filter = 0;
-
-                        $questions = DB::table('question')
-                                ->select('person.id as memberId', 'member.name', 'photo.url', 'publication.id', 'publication.date', 'question.title', 'publication.description', DB::raw('array_to_json(array_agg(tag.name)) tags'), DB::raw('COUNT(nullif(likes.likes, false)) likes'), DB::raw('COUNT(nullif(likes.likes, true)) dislikes'))
-                                ->where('question.title', 'ilike', '%' . $query . '%')
-                                ->orWhere('publication.description', 'ilike', '%' . $query . '%')
-                                ->join('publication', 'publication.id', '=', 'question.id_commentable_publication')
-                                ->join('person', 'publication.id_owner', '=', 'person.id')
-                                ->join('member', 'person.id', '=', 'member.id_person')
-                                ->leftJoin('photo', 'photo.id', '=', 'member.id_photo')
-                                ->leftJoin('tag_question', 'tag_question.id_question', '=', 'question.id_commentable_publication')
-                                ->leftJoin('tag', 'tag.id', "=", 'tag_question.id_tag')
-                                ->leftJoin('likes', 'likes.id_commentable_publication', '=', 'question.id_commentable_publication')
-                                ->groupBy('person.id', 'member.name', 'photo.url', 'publication.id', 'publication.date', 'question.title', 'publication.description')
-                                ->orderBy('likes', 'desc')
-                                ->orderBy('dislikes', 'desc')
-                                ->get();
-                } else if ($filter == 'recent') {
+                if ($filter == 'recent') {
                         $filter = 1;
 
                         $questions = DB::table('question')
@@ -290,6 +272,24 @@ class HomeController extends Controller
                                 ->groupBy('person.id', 'member.name', 'photo.url', 'publication.id', 'publication.date', 'question.title', 'publication.description')
                                 ->orderBy('dislikes', 'desc')
                                 ->orderBy('likes')
+                                ->get();
+                } else {
+                        $filter = 0;
+
+                        $questions = DB::table('question')
+                                ->select('person.id as memberId', 'member.name', 'photo.url', 'publication.id', 'publication.date', 'question.title', 'publication.description', DB::raw('array_to_json(array_agg(tag.name)) tags'), DB::raw('COUNT(nullif(likes.likes, false)) likes'), DB::raw('COUNT(nullif(likes.likes, true)) dislikes'))
+                                ->where('question.title', 'ilike', '%' . $query . '%')
+                                ->orWhere('publication.description', 'ilike', '%' . $query . '%')
+                                ->join('publication', 'publication.id', '=', 'question.id_commentable_publication')
+                                ->join('person', 'publication.id_owner', '=', 'person.id')
+                                ->join('member', 'person.id', '=', 'member.id_person')
+                                ->leftJoin('photo', 'photo.id', '=', 'member.id_photo')
+                                ->leftJoin('tag_question', 'tag_question.id_question', '=', 'question.id_commentable_publication')
+                                ->leftJoin('tag', 'tag.id', "=", 'tag_question.id_tag')
+                                ->leftJoin('likes', 'likes.id_commentable_publication', '=', 'question.id_commentable_publication')
+                                ->groupBy('person.id', 'member.name', 'photo.url', 'publication.id', 'publication.date', 'question.title', 'publication.description')
+                                ->orderBy('likes', 'desc')
+                                ->orderBy('dislikes', 'desc')
                                 ->get();
                 }
 
