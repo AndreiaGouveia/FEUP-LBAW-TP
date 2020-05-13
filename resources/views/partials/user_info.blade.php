6909b1246df196b490029bf85d4b4856f28573f3
up201704077 <up201704077@fe.up.pt>
2020-05-13 21:05:33 +0100

    Fixing user info

--- resources/views/partials/user_info.blade.php
+++ resources/views/partials/user_info.blade.php
@@ -26,13 +26,23 @@ $location = implode(",", $location_array);
         <h6><br></h6>
         <div class="profile_data">
             <h2><?=$member->name?><span class="badge badge-light"><i class="fas fa-shield-alt"></i></span></h2>
-            <p><span class="badge badge-light pl-0"> <i class="fas fa-address-book"></i> </span><?= $member->biography?></p>
-            <p><span class="badge badge-light pl-0"> <i class="fas fa-map-marker-alt"></i> </span><?=$location?></p>
-            <p><span class="badge badge-light pl-0"> <i class="fas fa-gem"></i> </span><?=$member->points?></p>
+
+            @if (isset($member->biography) && (trim($member->biography) != '') && ($member->biography != null ))
+            <p><span class="badge badge-light pl-0"> <i class="fas fa-address-book"></i> </span> {{ $member->biography }}</p>
+            @endif
+
+            @if (isset($location) && (trim($location) !== '') && ($location != null))
+            <p><span class="badge badge-light pl-0"> <i class="fas fa-map-marker-alt"></i> </span>{{ "|". $location . "|" }}</p>
+            @endif
+
+            @if (isset($member->points) && trim($member->points) != '' && $member->points != null )
+            <p><span class="badge badge-light pl-0"> <i class="fas fa-gem"></i> </span> {{ $member->points }}</p>
+            @endif
+
             <hr class="section-break" />
             <h5>Contribuições </h5>
-            <p><span class="badge badge-light pl-0"> <i class="fas fa-question"></i> </span><?=$member->questions?> Perguntas</p>
-            <p><span class="badge badge-light pl-0"> <i class="far fa-check-square"></i> </span><?=$member->reply?> Respotas</p>
-            <p><span class="badge badge-light pl-0"> <i class="far fa-comment"></i></span><?=$member->comments?> Comentários</p>
+            <p><span class="badge badge-light pl-0"> <i class="fas fa-question"></i> </span>{{ $member->questions->count() }} Perguntas</p>
+            <p><span class="badge badge-light pl-0"> <i class="far fa-check-square"></i> </span>{{ $member->answers->count() }} Respostas</p>
+            <p><span class="badge badge-light pl-0"> <i class="far fa-comment"></i></span>{{ $member->comments->count() }} Comentários</p>
         </div>
     </div>
