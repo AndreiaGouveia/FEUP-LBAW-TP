3eeede66088968fbb7ebb3db1001db463a25e63a
Margarida Alves Pinho <up201704599@fe.up.pt>
2020-06-03 18:07:04 +0100

    Update welcome.blade.php

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -4,7 +4,7 @@
         @component('mail::header', ['url' => config('app.url')])    
  
         <a class="navbar-brand px-2 mr-5" href="{{ url('home') }}">
-            <img src="https://i.ibb.co/FB6qj03/logo.png"  width="35" height="42" class="d-inline-block align-center" alt="logo" /></a>
+            <img src="https://i.ibb.co/FB6qj03/logo.png"  width="35" height="42" class="d-inline-block align-center" alt="logo" />
             Papagaio
         </a>
    
@@ -15,7 +15,7 @@
 
     <header class="welcome ">
             <h1 class="text-center">Bem-Vindo ao Papagaio!</h1>
-            <img src="https://i.ibb.co/FB6qj03/logo.png" alt="logo" /></a>
+            <img src="https://i.ibb.co/FB6qj03/logo.png" alt="logo" />
             <h2 class="text-center ">Entre no mundo do conhecimento Animal! 
                     Aqui vai poder esclarecer todas as suas duvidas e partilhar com os outros o seu conhecimento!!.</h2>
     </header>
