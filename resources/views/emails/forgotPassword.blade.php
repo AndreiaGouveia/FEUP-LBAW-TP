21ea377ff4ad4d5904eb8f7e75473f890ed789f1
Margarida Alves Pinho <up201704599@fe.up.pt>
2020-06-03 18:06:45 +0100

    Update forgotPassword.blade.php

--- resources/views/emails/forgotPassword.blade.php
+++ resources/views/emails/forgotPassword.blade.php
@@ -1,12 +1,11 @@
 
-
 @component('mail::layout')
     {{-- Header --}}
     @slot('header')
         @component('mail::header', ['url' => config('app.url')])    
  
         <a class="navbar-brand px-2 mr-5" href="{{ url('home') }}">
-            <img src="{{ asset('../images/logo.png') }}" width="35" height="42" class="d-inline-block align-center" alt="logo">
+            <img src="https://i.ibb.co/FB6qj03/logo.png"  width="35" height="42" class="d-inline-block align-center" alt="logo" />
             Papagaio
         </a>
    
@@ -17,7 +16,7 @@
 
     <header class="welcome ">
             <h1 class="text-center">Alterar palavra-passe!</h1>
-            <img src="{{ asset('../images/logo.png') }}" alt="logo" >
+            <img src="https://i.ibb.co/FB6qj03/logo.png" alt="logo" />
             <h2 class="text-center "><br>Olá! Já não te lembras da tua palavra-passe?
             <br> Não desesperes! Carrega no botão mágico para a repores!
             Obrigado por pertenceres a esta comunidade!!</h2>
